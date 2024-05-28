7e4e2752f724ea0b428d12400fe361c6038b0b00
Miroslav Stoyanov <miroslavstoyanov@deltasource.eu>
2024-05-28 19:52:37 +0300

    new simple step component, refactor scss

---
+++ src/app/feature/car-config/summary-step/summary-step.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-summary-step',
+  standalone: true,
+  imports: [],
+  templateUrl: './summary-step.component.html',
+  styleUrl: './summary-step.component.scss'
+})
+export class SummaryStepComponent {
+
+}
