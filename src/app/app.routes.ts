4de62610c599d447bfdf957c5d260658b57c6825
Miroslav Stoyanov <miroslavstoyanov@deltasource.eu>
2024-05-28 20:21:54 +0300

    add titles to routes, simplify css, refactor 'for' loop

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,14 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
     {
         path: 'step1',
+        title: 'Step 1',
         loadComponent: () => 
             import('./feature/car-config/model-and-color-step/model-and-color-step.component')
                 .then(c => c.ModelAndColorStepComponent)
     },
     {
         path: 'step2',
+        title: 'Step 2',
         loadComponent: () =>
         import('./feature/car-config/config-and-options-step/config-and-options-step.component')
             .then(c => c.ConfigAndOptionsStepComponent)
