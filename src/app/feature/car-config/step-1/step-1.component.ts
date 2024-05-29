22a2dea4170c02130ac805fa3c5ab8646ef5c605
Miroslav Stoyanov <miroslavstoyanov@deltasource.eu>
2024-05-29 10:57:01 +0300

    add image to step 1, improve readability

--- src/app/feature/car-config/step-1/step-1.component.ts
+++ src/app/feature/car-config/step-1/step-1.component.ts
@@ -6,7 +6,6 @@ import { Observable, Subscription } from 'rxjs';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { Color } from '../shared/color.model';
 import { Step1Form } from '../shared/step-1-form.model';
-import { AutoUnsubAdapter } from '../shared/auto-unsub-adapter';
 
 @Component({
   selector: 'app-step-1',
@@ -15,12 +14,12 @@ import { AutoUnsubAdapter } from '../shared/auto-unsub-adapter';
   templateUrl: './step-1.component.html',
   styleUrl: './step-1.component.scss'
 })
-export class Step1Component extends AutoUnsubAdapter implements OnInit {
-    vehicles$!: Observable<VehicleModel[]>;
-    step1Form!: FormGroup<Step1Form>;
+export class Step1Component implements OnInit {
+    protected readonly baseImageUrl: string = 'https://interstate21.com/tesla-app/images/';
+    protected vehicles$!: Observable<VehicleModel[]>;
+    protected step1Form!: FormGroup<Step1Form>;
 
     constructor(private teslaService: TeslaService) {
-      super();
       this.step1Form = new FormGroup({
         modelSelect: new FormControl<VehicleModel | null>(null),
         colorSelect: new FormControl<Color | null>(null)
@@ -31,7 +30,19 @@ export class Step1Component extends AutoUnsubAdapter implements OnInit {
       this.vehicles$ = this.teslaService.fetch();
     }
 
-    change(): void {
-      this.step1Form.patchValue({colorSelect: this.step1Form.get('modelSelect')?.value?.colors[0] || null});
+    protected fetchVehicleImageUrl(): string {
+      return this.baseImageUrl + this.modelSelect!.code + '/' + this.colorSelect!.code + '.jpg';
+    }
+
+    protected change(): void {
+      this.step1Form.patchValue({colorSelect: this.modelSelect?.colors[0] || null});
+    }
+
+    protected get modelSelect(): VehicleModel | null  {
+      return this.step1Form.get('modelSelect')?.value || null;
+    }
+
+    protected get colorSelect(): Color | null {
+      return this.step1Form.get('colorSelect')?.value || null;
     }
 }
