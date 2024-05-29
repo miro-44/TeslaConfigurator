438bdd70da71f8d7edaf388f02978d2e6a64759b
Miroslav Stoyanov <miroslavstoyanov@deltasource.eu>
2024-05-29 22:52:16 +0300

    add basic towHitch & yokeSteeringWheel UI

--- src/app/feature/car-config/step-2/step-2.component.ts
+++ src/app/feature/car-config/step-2/step-2.component.ts
@@ -3,10 +3,11 @@ import { Component, OnInit } from '@angular/core';
 import { Config } from '../shared/config.type';
 import { TeslaService } from '../shared/tesla.service';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
-import { Observable } from 'rxjs';
 import { FormStateTransferService } from '../shared/form-state-transfer.service';
 import { VehicleOptions } from '../shared/vehicle-options.type';
 import { UsdPipe } from '../shared/usd.pipe';
+import { AutoUnsubAdapter } from '../shared/auto-unsub-adapter';
+import { VehicleSetup } from '../shared/vehicle-setup.type';
 
 @Component({
   selector: 'app-step-2',
@@ -15,15 +16,18 @@ import { UsdPipe } from '../shared/usd.pipe';
   providers: [CurrencyPipe],
   templateUrl: './step-2.component.html'
 })
-export class Step2Component implements OnInit {
-  protected vehicleOptions$!: Observable<VehicleOptions>;
+export class Step2Component extends AutoUnsubAdapter implements OnInit {
+  protected vehicleOptions?: VehicleOptions;
+  protected currentVehicleSetup?: VehicleSetup;
   protected step2FormGroup!: FormGroup<{
     configSelect: FormControl<Config | null>,
     includeTowHitch: FormControl<boolean>,
     includeYoke: FormControl<boolean>
   }>;
 
-  constructor(private teslaService: TeslaService, private formStateTransferService: FormStateTransferService) {}
+  constructor(private teslaService: TeslaService, private formStateTransferService: FormStateTransferService) {
+    super();
+  }
 
   ngOnInit(): void {
     this.step2FormGroup = new FormGroup({
@@ -31,10 +35,21 @@ export class Step2Component implements OnInit {
       includeTowHitch: new FormControl<boolean>(false, {nonNullable: true}),
       includeYoke: new FormControl<boolean>(false, {nonNullable: true})
     });
-    this.vehicleOptions$ = this.teslaService.fetchConfigs(this.formStateTransferService.getModelAndColor()!.model.code);
+    this.subs.add(
+      this.teslaService.fetchConfigs(this.formStateTransferService.getModelAndColor()!.model.code)
+        .subscribe(vehicleOptions => this.vehicleOptions = vehicleOptions)
+    );
   }
 
   protected get configSelect(): Config | null {
     return this.step2FormGroup.controls.configSelect.value;
+  }
+
+  protected get includeTowHitch(): boolean {
+    return this.step2FormGroup.controls.includeTowHitch.value;
+  }
+
+  protected get includeYoke(): boolean {
+    return this.step2FormGroup.controls.includeYoke.value;
   }
 }
