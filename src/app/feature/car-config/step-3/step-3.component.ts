2b82e607ab04fe7bddcf7b5a37163f33a960c833
Miroslav Stoyanov <miroslavstoyanov@deltasource.eu>
2024-05-30 00:54:23 +0300

    add step 3 minimal summary

--- src/app/feature/car-config/step-3/step-3.component.ts
+++ src/app/feature/car-config/step-3/step-3.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { FormStateTransferService } from '../shared/form-state-transfer.service';
+import { ModelAndColor } from '../shared/model-and-color.type';
 
 @Component({
   selector: 'app-step-3',
   standalone: true,
   imports: [],
-  templateUrl: './step-3.component.html',
-  styleUrl: './step-3.component.scss'
+  templateUrl: './step-3.component.html'
 })
 export class Step3Component {
+
+  constructor(private formStateTransferService: FormStateTransferService) {}
+
+  get modelAndColor(): ModelAndColor {
+    return this.formStateTransferService.modelAndColor!;
+  }
 
 }
