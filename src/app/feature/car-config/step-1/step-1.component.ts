845656a78f624d46988e1523ed91424c0881cfea
Miroslav Stoyanov <miroslavstoyanov@deltasource.eu>
2024-05-29 08:56:05 +0300

    transition to reactive forms

--- src/app/feature/car-config/step-1/step-1.component.ts
+++ src/app/feature/car-config/step-1/step-1.component.ts
@@ -2,28 +2,32 @@ import { Component, OnInit } from '@angular/core';
 import { TeslaService } from '../shared/tesla.service';
 import { VehicleModel } from '../shared/vehicle-model.model';
 import { AsyncPipe, CommonModule } from '@angular/common';
-import { Observable } from 'rxjs';
-import { FormsModule } from '@angular/forms';
+import { Observable, Subscription } from 'rxjs';
+import { Form, FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Color } from '../shared/color.model';
 
 @Component({
   selector: 'app-step-1',
   standalone: true,
-  imports: [AsyncPipe, FormsModule],
+  imports: [AsyncPipe, ReactiveFormsModule],
   templateUrl: './step-1.component.html',
   styleUrl: './step-1.component.scss'
 })
 export class Step1Component implements OnInit {
     vehicles!: Observable<VehicleModel[]>;
-    selectedVehicle?: VehicleModel;
+    step1Form!: FormGroup;
+    modelSelect: FormControl<VehicleModel | null> = new FormControl(null);
+    colorSelect: FormControl<Color | null> = new FormControl(null);
 
-    constructor(private teslaService: TeslaService) {}
+    constructor(private teslaService: TeslaService) {
+      this.step1Form = new FormGroup({
+        modelSelect: this.modelSelect,
+        colorSelect: this.colorSelect
+      });
+    }
 
     ngOnInit(): void {
       this.vehicles = this.teslaService.fetch();
     }
 
-    selectVehicle(selectedVehicle: VehicleModel): void {
-      console.log("Selected: " + this.selectedVehicle);
-      this.selectedVehicle = selectedVehicle;
-    }
 }
