a289f0f623a7a6bc3b2ea90524e0f77cc6bf8ceb
Miroslav Stoyanov <miroslavstoyanov@deltasource.eu>
2024-05-29 18:27:59 +0300

    add extra component layer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,11 @@
-import {Component} from '@angular/core';
-import {AsyncPipe, JsonPipe} from '@angular/common';
-import { RouterModule, Routes } from '@angular/router';
-import { NavbarComponent } from './core/navbar/navbar.component';
-import { routes } from './app.routes';
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { FormContainerComponent } from './feature/car-config/form-container/form-container.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [AsyncPipe, JsonPipe, RouterModule, NavbarComponent],
+  imports: [RouterModule, FormContainerComponent],
   templateUrl: './app.component.html'
 })
 export class AppComponent {
---
+++ src/app/feature/car-config/form-container/form-container.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { NavbarComponent } from '../../../core/navbar/navbar.component';
+
+@Component({
+  selector: 'app-form-container',
+  standalone: true,
+  imports: [NavbarComponent],
+  templateUrl: './form-container.component.html',
+  styleUrl: './form-container.component.scss'
+})
+export class FormContainerComponent {
+
+}
