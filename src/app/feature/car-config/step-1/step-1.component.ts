011d758015b8726038b0c7bc6410c8685bd393da
Miroslav Stoyanov <miroslavstoyanov@deltasource.eu>
2024-05-29 07:58:29 +0300

    add simple template-driven form for step 1

--- src/app/feature/car-config/step-1/step-1.component.ts
+++ src/app/feature/car-config/step-1/step-1.component.ts
@@ -1,22 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 import { TeslaService } from '../shared/tesla.service';
 import { VehicleModel } from '../shared/vehicle-model.model';
-import { AsyncPipe } from '@angular/common';
+import { AsyncPipe, CommonModule } from '@angular/common';
 import { Observable } from 'rxjs';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-step-1',
   standalone: true,
-  imports: [AsyncPipe],
+  imports: [AsyncPipe, FormsModule],
   templateUrl: './step-1.component.html',
   styleUrl: './step-1.component.scss'
 })
 export class Step1Component implements OnInit {
     vehicles!: Observable<VehicleModel[]>;
+    selectedVehicle?: VehicleModel;
 
     constructor(private teslaService: TeslaService) {}
 
     ngOnInit(): void {
       this.vehicles = this.teslaService.fetch();
+    }
+
+    selectVehicle(selectedVehicle: VehicleModel): void {
+      console.log("Selected: " + this.selectedVehicle);
+      this.selectedVehicle = selectedVehicle;
     }
 }
