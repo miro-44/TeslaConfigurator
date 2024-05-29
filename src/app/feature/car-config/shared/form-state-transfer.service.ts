f97bb4385ee62dc0e5bd32f3d15034c5d3e8b646
Miroslav Stoyanov <miroslavstoyanov@deltasource.eu>
2024-05-30 00:13:17 +0300

    further cleanup, remove unneeded sessionStorage entirely

--- src/app/feature/car-config/shared/form-state-transfer.service.ts
+++ src/app/feature/car-config/shared/form-state-transfer.service.ts
@@ -12,6 +12,8 @@ export class FormStateTransferService {
 
   constructor() { }
 
+  configAndExtras: ConfigAndExtras | null = null;
+
   set modelAndColor(modelAndColor: ModelAndColor | null) {
     this.modelAndColor$.next(modelAndColor);
   }
@@ -24,23 +26,4 @@ export class FormStateTransferService {
     return this.modelAndColor$.subscribe(subscriber);
   }
 
-  setConfigAndExtras(configAndExtras: ConfigAndExtras | null): void {
-    if (!configAndExtras) {
-      sessionStorage.removeItem('step2');
-      return;
-    }
-    sessionStorage.setItem('step2', JSON.stringify(configAndExtras));
-  }
-
-  getConfigAndExtras(): ConfigAndExtras | null {
-    return this.retrieveItem<ConfigAndExtras>('step2');
-  }
-
-  private retrieveItem<T>(key: string): T | null {
-    const itemAsString: string | null = sessionStorage.getItem(key);
-    if (!itemAsString) {
-      return null;
-    }
-    return JSON.parse(itemAsString) as T;
-  }
 }
