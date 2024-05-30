68026463ff9570f76637fcd48effe3074ef5d30f
Miroslav Stoyanov <miroslavstoyanov@deltasource.eu>
2024-05-30 14:15:48 +0300

    add autotoggle for buttons

--- src/app/core/navbar/navbar.component.ts
+++ src/app/core/navbar/navbar.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, Signal, computed, signal } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { routes } from '../../app.routes';
+import { FormStateTransferService } from '../../feature/car-config/shared/form-state-transfer.service';
 
 @Component({
   selector: 'app-navbar',
@@ -10,5 +11,22 @@ import { routes } from '../../app.routes';
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
-  appRoutes: Routes = routes;
+  protected stepRoutes: Routes = routes;
+
+  constructor(private formStateTransferService: FormStateTransferService) {}
+
+  protected readonly stepRouteValidMap: {path: string, valid: Signal<boolean>}[] = [
+    {
+      path: 'step1',
+      valid: signal(true)
+    },
+    {
+      path: 'step2',
+      valid: computed(() => this.formStateTransferService.modelAndColorState.valid)
+    },
+    {
+      path: 'step3',
+      valid: computed(() => this.formStateTransferService.configAndExtrasState.valid)
+    }
+  ]
 }
