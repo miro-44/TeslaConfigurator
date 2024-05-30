de312e05b4c7f6e813eb8cc9555b355b4c26ec1a
Miroslav Stoyanov <miroslavstoyanov@deltasource.eu>
2024-05-30 18:18:36 +0300

    add missing access modifier

--- src/app/feature/steps/shared/auto-unsub-adapter.class.ts
+++ src/app/feature/steps/shared/auto-unsub-adapter.class.ts
@@ -3,7 +3,7 @@ import { Subscription } from "rxjs";
 
 @Directive({})
 export class AutoUnsubAdapter implements OnDestroy {
-    subs: Subscription = new Subscription();
+    protected subs: Subscription = new Subscription();
 
     ngOnDestroy(): void {
         this.subs.unsubscribe();
